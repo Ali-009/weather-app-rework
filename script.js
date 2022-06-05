0bdb49c79dba76d3fe027af8bc82c9d439bc0573
AliTinai <ali-009@windowslive.com>
2022-06-05 12:58:57 +0400

    Tested the basic logic for fetching data from the weather API

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 async function getWeatherData(locationInput){
 
     let apiKey = '9ccfde44cd99c120bad6a7b986a92fb2';
@@ -13,11 +11,19 @@ async function getWeatherData(locationInput){
   
     let data = await response.json();
   
-    let weatherKelvin = data.main.temp;
+    /*let weatherKelvin = data.main.temp;
     let weatherCelsius = Math.round(weatherKelvin - 273.15);
     let weatherFahrenheit = Math.round(weatherKelvin * (9/5) - 459.67);
   
     let weatherDescription = data.weather[0].description;
   
-    return {weatherCelsius, weatherFahrenheit, weatherDescription};
+    return {weatherCelsius, weatherFahrenheit, weatherDescription};*/
+
+    return data;
 }
+
+/*To constantly update the UI, we need an asynchronous function*/
+//For that to happen, we need to keep calling getWeatherData in a loop
+//Data Containers is an array of elements onto which we display the data we are asynchronously fetching
+
+
